6e7ada69e6ce336f9be093f38e968367061bc2d4
Bladimir  Rosario Butten <b.rosario@sisalril.gov.do>
2018-03-21 16:33:01 -0400

    Primer Commit

---
+++ src/app/app.route.ts
@@ -0,0 +1,29 @@
+import { RegistrerComponent } from './login/registrer.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import {Routes, RouterModule} from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import {PagesComponent  } from './pages/pages.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'registrer', component: RegistrerComponent },
+
+  { path: '**', component: NopagefoundComponent },
+];
+
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true} );
