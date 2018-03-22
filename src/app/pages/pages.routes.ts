9088983e74edd17d9080a3d69d88caf859326535
Bladimir  Rosario Butten <b.rosario@sisalril.gov.do>
2018-03-22 10:37:20 -0400

    Agregamos modulos

---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,24 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+
+const pagesRoutes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+
+];
+
+
+export const PagesRoutingModule = RouterModule.forChild(pagesRoutes);
